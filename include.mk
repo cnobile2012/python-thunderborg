4ae7a631ba8b692105ad42464aa4db20f2ebe86c
Carl J. Nobile <carl.nobile@gmail.com>
2026-05-20 12:40:49 -0400

    Bumped version.

--- include.mk
+++ include.mk
@@ -3,9 +3,9 @@
 #
 
 # Django Dynamic Column version info.
-MAJORVERSION	= 1
+MAJORVERSION	= 2
 MINORVERSION	= 0
-PATCHLEVEL	= 2
+PATCHLEVEL	= 0
 VERSION		= ${MAJORVERSION}.${MINORVERSION}.${PATCHLEVEL}
 # How mature is this project? Common values are
 #   3 - Alpha
